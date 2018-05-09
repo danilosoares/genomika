b4e54e04f1a2fd6e6cb26cc57b4c21a460ca3d80
Nielson Santana <nielsonnas@gmail.com>
2018-05-09 12:44:57 -0300

    Project working, but with bug in search

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
-**#Dependência a ser instalada
-**
+**# Dependência a ser instalada**
+
 psycopg2 - pip install psycopg2
 
-**#Tarefa 1
-**
+**# Tarefa 1**
 
 O modelo não está adequado para o problema. Como é possível uma doença estar associada a vários genes e um gene pode estar relacionado com várias doenças **(Vide exemplo no PDF do processo seletivo)**, é necessário implementar um relacionamento m:n, onde uma doença possui vários genes e um gene pode estar relacionado a várias doenças.
 
--- phenos/views.py
+++ phenos/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from django.template.response import TemplateResponse
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
@@ -12,78 +13,89 @@ from django import forms
 
 # Create your views here.
 
+
 def index(request):
-	if 'user_id' not in request.session:
-  		template = loader.get_template('phenos/login.html')
-		return HttpResponse(template.render(request))
-   
-	template = loader.get_template('phenos/index.html')
-	return HttpResponse(template.render(request))
+    if 'user_id' not in request.session:
+        template = loader.get_template('phenos/login.html')
+        return HttpResponse(template.render({}, request))
+
+    template_name = 'phenos/index.html'
+    return TemplateResponse(request, template_name)
+
 
 def results(request):
-	if 'user_id' not in request.session:
-		template = loader.get_template('phenos/login.html')
-		return HttpResponse(template.render(request))
-	term = request.GET['term']
-	diseases = Disease.objects.filter(name__in=term.split(','))
-	
-	template = loader.get_template('phenos/results.html')
-	c = Context({'term' : term, 'diseases' : diseases })
-	return HttpResponse(template.render(c))
+    if 'user_id' not in request.session:
+        template = loader.get_template('phenos/login.html')
+        return HttpResponse(template.render({}, request))
+    term = request.POST['term']
+    diseases = Disease.objects.filter(name__in=term.split(','))
+
+    template = loader.get_template('phenos/results.html')
+    c = Context({'term': term, 'diseases': diseases})
+    return HttpResponse(template.render({}, c))
+
 
 def diseases(request):
-	if 'user_id' not in request.session:
-		template = loader.get_template('phenos/login.html')
-		return HttpResponse(template.render(request))
-	term = request.GET['term']
+    if 'user_id' not in request.session:
+        template = loader.get_template('phenos/login.html')
+        return HttpResponse(template.render({}, request))
+    term = request.GET['term']
 
-	diseases = Disease.objects.filter(name__contains=term)
-	data = serializers.serialize('json', diseases)
-	return HttpResponse(data, content_type='application/json')
+    diseases = Disease.objects.filter(name__contains=term)
+    data = serializers.serialize('json', diseases)
+    return HttpResponse(data, content_type='application/json')
+
 
 def login(request):
-	if 'user_id' not in request.session:
-		template = loader.get_template('phenos/login.html')
-		return HttpResponse(template.render(request))
-	template = loader.get_template('phenos/index.html')
-	return HttpResponse(template.render(request))
+    if 'user_id' not in request.session:
+        template = loader.get_template('phenos/login.html')
+        return HttpResponse(template.render({}, request))
+    template = loader.get_template('phenos/index.html')
+    return HttpResponse(template.render({}, request))
+
 
 def registration(request):
-	template = loader.get_template('phenos/registration.html')
-	return HttpResponse(template.render(request))
+    template = loader.get_template('phenos/registration.html')
+    return HttpResponse(template.render({}, request))
+
 
 def user_create(request):
-	if not request.POST['user_email']:
-		raise forms.ValidationError("Email não pode ficar em branco!")
-	if not request.POST['user_password']:
-		raise forms.ValidationError("Senha não pode ficar em branco!")
+    email = request.POST['user_email']
+    if not email:
+        raise forms.ValidationError("Email não pode ficar em branco!")
+    if not email.find('@'):
+        raise forms.ValidationError(
+            "Use um email válido para criar o usuário!")
+    if not request.POST['user_password']:
+        raise forms.ValidationError("Senha não pode ficar em branco!")
 
-	user_email = request.POST['user_email']
-	user_password = request.POST['user_password']
-	user = User(email=user_email,password=user_password)
-	created_user = user.save()
-	request.session['user_id'] = created_user.id
-	template = loader.get_template('phenos/index.html')
-	return HttpResponse(template.render(request))
+    user_email = request.POST['user_email']
+    user_password = request.POST['user_password']
+    user = User(email=user_email, password=user_password)
+    user.save()
+    request.session['user_id'] = user.id
+    template = loader.get_template('phenos/index.html')
+    return HttpResponse(template.render({}, request))
+
 
 def authenticate(request):
     if request.method != 'POST':
         raise Http404('Only POSTs are allowed')
     try:
         user = User.objects.get(email=request.POST['user_email'])
+        template = loader.get_template('phenos/index.html')
         if user.password == request.POST['user_password']:
             request.session['user_id'] = user.id
-            template = loader.get_template('phenos/index.html')
-    	return HttpResponse(template.render(request))
+        return HttpResponse(template.render({}, request))
     except User.DoesNotExist:
         template = loader.get_template('phenos/login.html')
-	return HttpResponse(template.render(request))
+        return HttpResponse(template.render({}, request))
+
 
 def logout(request):
-	try:
-		del request.session['user_id']
-	except KeyError:
-		pass
-	template = loader.get_template('phenos/login.html')
-	return HttpResponse(template.render(request))
-
+    try:
+        del request.session['user_id']
+    except KeyError:
+        pass
+    template = loader.get_template('phenos/login.html')
+    return HttpResponse(template.render({}, request))
